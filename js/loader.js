7526c15b83efc628f72b9f630ba9dd45240d0a07
Padigrim <36322443+Padigrim@users.noreply.github.com>
2022-08-03 20:17:48 -0400

    Add files via upload

--- js/loader.js
+++ js/loader.js
@@ -1,7 +1,15 @@
 var animation = bodymovin.loadAnimation({
-    container: document.getElementById('loader_1'),
+    container: document.getElementById('loaderAnim'),
     renderer: 'svg',
     loop: true,
     autoplay: true,
     path: 'loader.json'
+});
+
+window.addEventListener('load', (event) => {
+    let loader = document.getElementById('loader');
+
+    setTimeout (function() {
+        loader.style.display = "none";
+    }, 5120);
 })
