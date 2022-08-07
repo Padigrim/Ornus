a2eeeb68c30cfab247a55f1b078d987d6248dc40
Padigrim <36322443+Padigrim@users.noreply.github.com>
2022-08-07 03:33:32 -0400

    Loader Revamp
    Changed the loader animation

--- js/loader.js
+++ js/loader.js
@@ -1,4 +1,4 @@
-var animation = bodymovin.loadAnimation({
+var animation1 = bodymovin.loadAnimation({
     container: document.getElementById('loaderAnim'),
     renderer: 'svg',
     loop: true,
@@ -6,14 +6,28 @@ var animation = bodymovin.loadAnimation({
     path: 'loader.json'
 });
 
+/* You might need to delete this if nothing works */
+
+var animation2 = bodymovin.loadAnimation({
+    container: document.getElementById('loaderAnim1'),
+    renderer: 'svg',
+    loop: false,
+    autoplay: true,
+    path: 'loaderEnd.json'
+});
+
+var loader1 = document.getElementById('loaderAnim')
+var loader2 = document.getElementById('loaderAnim1')
 var loader = document.getElementById('loader');
 
 window.addEventListener('load', (event) => {
+    loader1.style.display = "none";
+    loader2.style.display = "inline";
     setTimeout (function() {
         loader.style.transition = '.5s';
         loader.style.opacity = '0';
         setTimeout (function() {
             loader.style.display = "none";
         }, 500);
-    }, 5120);
-})
+    }, 2070);
+});
