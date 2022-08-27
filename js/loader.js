caa354e402de324c269ade4f83bc0777b7599295
Padigrim <36322443+Padigrim@users.noreply.github.com>
2022-08-26 22:40:55 -0400

    Testing

--- js/loader.js
+++ js/loader.js
@@ -7,14 +7,19 @@ var animation1 = bodymovin.loadAnimation({
 });
 
 var loader = document.getElementById('loader');
+var noticeVisibility = document.getElementsByClassName('noticeVisibility');
+
+/* remove the switch from loaderEnd as it does not work properly. Save it as an animation for something else */
 
 window.addEventListener('load', (event) => {
-    animation1.path = 'loaderEnd.json'
     setTimeout (function() {
         loader.style.transition = '.5s';
         loader.style.opacity = '0';
         setTimeout (function() {
             loader.style.display = "none";
+            setTimeout (function() {
+                noticeVisibility.style.display = "flex";
+            }, 500);
         }, 500);
-    }, 2070);
+    }, 5000);
 });
